8f30c2067948c2a99baf5c8536b382c86f72bb8f
abdullah-fikri <abdullahfikri981.com>
2025-11-10 00:17:50 +0700

    feat: added controllers login

--- controllers/auth_controller.go
+++ controllers/auth_controller.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"backend/lib"
 	"backend/models"
 
 	"github.com/gin-gonic/gin"
@@ -31,4 +32,45 @@ func RegisterUser(ctx *gin.Context) {
 		Message: "Register success",
 		Data:    user,
 	})
+}
+
+func LoginUser(ctx *gin.Context) {
+	var req models.LoginRequest
+
+	if err := ctx.BindJSON(&req); err != nil {
+		ctx.JSON(400, models.Response{
+			Success: false,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	user, err := models.Login(req.Email)
+	if err != nil {
+		ctx.JSON(400, models.Response{
+			Success: false,
+			Message: "wrong email or password",
+		})
+		return
+	}
+
+	if !lib.VerifyPassword(req.Password, user.Password) {
+		ctx.JSON(400, models.Response{
+			Success: false,
+			Message: "wrong email or password",
+		})
+		return
+	}
+	intId := int(user.ID)
+	token := lib.GeneratedTokens(intId, user.Role)
+
+	ctx.JSON(200, models.Response{
+		Success: true,
+		Message: "Login success",
+		Data: map[string]any{
+			"user":  user,
+			"token": token,
+		},
+	})
+
 }
