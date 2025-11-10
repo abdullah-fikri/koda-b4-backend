0cba2362868485f13f8c9f5abc433819b7204602
abdullah-fikri <abdullahfikri981.com>
2025-11-10 11:58:17 +0700

    feat; added update data user

--- controllers/auth_controller.go
+++ controllers/auth_controller.go
@@ -74,3 +74,90 @@ func LoginUser(ctx *gin.Context) {
 	})
 
 }
+func UpdateUser(ctx *gin.Context) {
+	var req models.RegisterRequest
+	if err := ctx.BindJSON(&req); err != nil {
+		ctx.JSON(400, models.Response{
+			Success: false,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	u, err := models.UpdateUser(req.Email, req)
+	if err != nil {
+		ctx.JSON(500, models.Response{
+			Success: false,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(200, models.Response{
+		Success: true,
+		Message: "update succesfully",
+		Data:    u,
+	})
+}
+
+func ForgotUSer(ctx *gin.Context) {
+	var req models.LoginRequest
+
+	if err := ctx.BindJSON(&req); err != nil {
+		ctx.JSON(400, models.Response{
+			Success: false,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	user, err := models.Forgot(req.Email)
+	if err != nil {
+		ctx.JSON(400, models.Response{
+			Success: false,
+			Message: "email not found",
+		})
+		return
+	}
+
+	intId := int(user.ID)
+	token := lib.GeneratedTokens(intId, user.Role)
+
+	ctx.JSON(200, models.Response{
+		Success: true,
+		Message: "account reset success",
+		Data: map[string]any{
+			"user":  user,
+			"token": token,
+		},
+	})
+}
+
+//admin
+
+func RegisterAd(ctx *gin.Context) {
+	var req models.RegisterRequest
+
+	if err := ctx.BindJSON(&req); err != nil {
+		ctx.JSON(400, models.Response{
+			Success: false,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	user, err := models.RegisterAd(req)
+	if err != nil {
+		ctx.JSON(400, models.Response{
+			Success: false,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(200, models.Response{
+		Success: true,
+		Message: "Register success",
+		Data:    user,
+	})
+}
