fe6722d44ed96d2b6f81496bdf11bdbd412e9f3d
abdullah-fikri <abdullahfikri981.com>
2025-11-10 13:06:01 +0700

    feat: router for auth

--- routes/auth_router.go
+++ routes/auth_router.go
@@ -2,11 +2,20 @@ package routes
 
 import (
 	"backend/controllers"
+	"backend/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func AuthRoutes(r *gin.Engine) {
+	admin := r.Group("/admin")
+	admin.Use(middleware.Auth())
+	admin.Use(middleware.AdminOnly())
+
+	admin.POST("/register", controllers.RegisterAd)
+	admin.POST("/update", controllers.UpdateUserAd)
+
 	r.POST("/auth/register", controllers.RegisterUser)
 	r.POST("/auth/login", controllers.LoginUser)
+	r.PUT("/auth/update", controllers.UpdateUser)
 }
