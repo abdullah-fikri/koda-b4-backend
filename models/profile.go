d194fc706ed135df488c3e934e03d2f779f911f4
abdullah-fikri <abdullahfikri981.com>
2025-11-10 14:04:57 +0700

    feat: added models for upload picture

--- models/profile.go
+++ models/profile.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"backend/config"
+	"context"
+)
+
 type Profile struct {
 	ID        int64  `json:"id"`
 	UsersID   int64  `json:"users_id"`
@@ -8,4 +13,17 @@ type Profile struct {
 	Address   string `json:"address"`
 	CreatedAt string `json:"created_at"`
 	UpdatedAt string `json:"updated_at"`
+}
+
+func UpdateProfilePicture(id string, path string) error {
+	ctx := context.Background()
+
+	_, err := config.Db.Exec(ctx,
+		`UPDATE profile
+		 SET profile_picture = $1
+		 WHERE users_id = $2`,
+		path, id,
+	)
+
+	return err
 }
