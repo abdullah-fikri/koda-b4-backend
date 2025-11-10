e2428316024a670830f5f8e631a8c1bea8bbc9e6
abdullah-fikri <abdullahfikri981.com>
2025-11-10 10:51:25 +0700

    feat: added router delete products

--- routes/product_router.go
+++ routes/product_router.go
@@ -10,11 +10,12 @@ import (
 func ProductRouter(r *gin.Engine) {
 
 	admin := r.Group("/admin")
-	admin.Use(middleware.Auth()) 
-	admin.Use(middleware.AdminOnly())    
+	admin.Use(middleware.Auth())
+	admin.Use(middleware.AdminOnly())
 
 	admin.POST("/product", controllers.CreateProduct)
 	admin.PUT("/product/:id", controllers.UpdateProduct)
+	admin.DELETE("/product/:id", controllers.DeleteProduct)
 
 	r.GET("/products", controllers.Product)
 	r.GET("/products/:id", controllers.ProductDetail)
