a7893de09a4e62f3e863417a891c0e5a4d28d3f5
abdullah-fikri <abdullahfikri981.com>
2025-11-11 16:49:35 +0700

    feat: added pagination and save it in redis

--- controllers/favorite.go
+++ controllers/favorite.go
@@ -1,8 +1,12 @@
 package controllers
 
 import (
+	"backend/config"
 	"backend/models"
+	"context"
+	"encoding/json"
 	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -20,6 +24,22 @@ func FavoriteProduct(ctx *gin.Context) {
 	if err != nil || limitInt < 1 {
 		limitInt = 4
 	}
+	key := ctx.Request.RequestURI
+	var cacheData struct {
+		Products   []models.Product `json:"products"`
+		Pagination map[string]any   `json:"pagination"`
+	}
+
+	cache, err := config.Rdb.Get(context.Background(), key).Result()
+	if err == nil && cache != "" {
+		_ = json.Unmarshal([]byte(cache), &cacheData)
+		ctx.JSON(200, models.Response{
+			Success: true,
+			Message: "list favorite products ( from cache )",
+			Data:    cacheData,
+		})
+		return
+	}
 
 	products, total, err := models.Favorite(pageInt, limitInt)
 	if err != nil {
@@ -29,19 +49,34 @@ func FavoriteProduct(ctx *gin.Context) {
 		})
 		return
 	}
+	totalPage := int((total + int64(limitInt) - 1) / int64(limitInt))
+
+	cacheData = struct {
+		Products   []models.Product `json:"products"`
+		Pagination map[string]any   `json:"pagination"`
+	}{
+		Products: products,
+		Pagination: map[string]any{
+			"page":       page,
+			"limit":      limit,
+			"total_data": total,
+			"total_page": totalPage,
+		},
+	}
+
+	jsonData, err := json.Marshal(cacheData)
+	if err != nil {
+		ctx.JSON(400, models.Response{
+			Success: false,
+			Message: "failed marshal data",
+		})
+		return
+	}
+	config.Rdb.Set(context.Background(), key, jsonData, 15*time.Minute)
 
 	ctx.JSON(200, models.Response{
 		Success: true,
-		Message: "list favorite products",
-		Data: models.Response{
-			Success: true,
-			Message: "list favorite products",
-			Data: map[string]any{
-				"products": products,
-				"total":    total,
-				"page":     pageInt,
-				"limit":    limitInt,
-			},
-		},
+		Message: "list products favorite",
+		Data:    cacheData,
 	})
 }
