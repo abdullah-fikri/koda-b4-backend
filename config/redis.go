ff63014f8b1d5ed556ebca6c95ad42cc8ae60c02
abdullah-fikri <abdullahfikri981.com>
2025-11-11 14:40:00 +0700

    fix: use env for important data

--- config/redis.go
+++ config/redis.go
@@ -12,9 +12,10 @@ var Rdb *redis.Client
 func Redis() {
 	godotenv.Load()
 	redisUrl := os.Getenv("REDIS_URL")
+	redisPassword := os.Getenv("PASSWORD_REDIS")
 	Rdb = redis.NewClient(&redis.Options{
 		Addr:     redisUrl,
-		Password: "",
+		Password: redisPassword,
 		DB:       0,
 	})
 }
