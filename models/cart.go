0ab3368712e873874cc970005c0eaf8a41d15cba
abdullah-fikri <abdullahfikri981.com>
2025-11-12 14:27:26 +0700

    fix: no longer take subtotal from database, but directly calculate in query

--- models/cart.go
+++ models/cart.go
@@ -60,39 +60,24 @@ func AddToCart(req ReqCart) (int64, error) {
 		  AND COALESCE(size_id, 0) = COALESCE($4::BIGINT, 0)
 	`, cartID, req.ProductID, req.VariantID, req.SizeID).Scan(&existingID)
 
-	var price float64
-	if req.SizeID != nil {
-		err = config.Db.QueryRow(ctx, `
-			SELECT price FROM product_size WHERE id = $1
-		`, req.SizeID).Scan(&price)
-	} else {
-		err = config.Db.QueryRow(ctx, `
-			SELECT MIN(price) FROM product_size WHERE product_id = $1
-		`, req.ProductID).Scan(&price)
-	}
-	if err != nil {
-		return 0, err
-	}
-
 	// update
 	if checkErr == nil {
 		_, err = config.Db.Exec(ctx, `
 			UPDATE cart_items
-			SET qty = qty + $1::INT,
-			    subtotal = (qty + $1::INT) * $2::NUMERIC
-			WHERE id = $3::BIGINT
-		`, req.Qty, price, existingID)
+			SET qty = qty + $1::INT
+			WHERE id = $2::BIGINT
+		`, req.Qty, existingID)
 		if err != nil {
 			return 0, err
 		}
 		return cartID, nil
 	}
 
-	//  buat baru
+	// buat baru
 	_, err = config.Db.Exec(ctx, `
-		INSERT INTO cart_items (cart_id, product_id, variant_id, size_id, qty, subtotal)
-		VALUES ($1::BIGINT, $2::BIGINT, $3::BIGINT, $4::BIGINT, $5::INT, $6::NUMERIC * $5::NUMERIC)
-	`, cartID, req.ProductID, req.VariantID, req.SizeID, req.Qty, price)
+		INSERT INTO cart_items (cart_id, product_id, variant_id, size_id, qty)
+		VALUES ($1::BIGINT, $2::BIGINT, $3::BIGINT, $4::BIGINT, $5::INT)
+	`, cartID, req.ProductID, req.VariantID, req.SizeID, req.Qty)
 	if err != nil {
 		return 0, err
 	}
@@ -111,7 +96,7 @@ func GetCart(userID int64) ([]CartItemResponse, error) {
 			COALESCE(v.name, '') AS variant_name,
 			COALESCE(s.name, '') AS size_name,
 			ps.price AS price,
-			ci.subtotal,
+			(ps.price * ci.qty) AS subtotal, 
 			ci.qty,
 			(
 				SELECT pi.image 
