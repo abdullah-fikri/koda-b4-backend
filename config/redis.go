cda8b32af8c6bc70a0391af9f35f454e1eb8e2d4
abdullah-fikri <abdullahfikri981@gmail.com>
2025-11-19 07:10:50 +0000

    refactor: db load from env

--- config/redis.go
+++ config/redis.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"os"
+	"strconv"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -11,9 +12,11 @@ var Rdb *redis.Client
 func Redis() {
 	redisUrl := os.Getenv("REDIS_URL")
 	redisPassword := os.Getenv("PASSWORD_REDIS")
+	dbRedis := os.Getenv("DB")
+	dbFinal,_ := strconv.Atoi(dbRedis)
 	Rdb = redis.NewClient(&redis.Options{
 		Addr:     redisUrl,
 		Password: redisPassword,
-		DB:       0,
+		DB:       dbFinal,
 	})
 }
