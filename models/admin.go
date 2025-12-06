b73e0966b6ab64fb693315a61513769483840cdf
abdullah-fikri <abdullahfikri981@gmail.com>
2025-12-06 07:50:56 +0000

    feat: add crud for category by admin

--- models/admin.go
+++ models/admin.go
@@ -13,6 +13,11 @@ type OrderListItem struct {
 	Total  float64   `json:"total"`
 }
 
+type Categories struct {
+	Id int `json:id`
+	Name string `json:"name" binding:"required"`
+}
+
 type UpdateOrderStatusRequest struct {
 	Status int `json:"status"`
 }
@@ -65,5 +70,76 @@ func UpdateOrderStatus(orderID int64, status int) error {
 		return err
 	}
 
+	return err
+}
+
+
+func CreateCategory(name string) (Categories, error) {
+    ctx := context.Background()
+
+    var c Categories
+
+    err := config.Db.QueryRow(ctx,
+        "INSERT INTO categories (name) VALUES ($1) RETURNING name",
+        name,
+    ).Scan(&c.Name)
+
+    if err != nil {
+        return Categories{}, err
+    }
+
+    return c, nil
+}
+
+func GetAllCategories() ([]Categories, error) {
+	ctx := context.Background()
+	query := "SELECT id,name FROM categories ORDER BY id DESC"
+
+	rows, err := config.Db.Query(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var categories []Categories
+	for rows.Next(){
+		var c Categories
+		err = rows.Scan(&c.Id,&c.Name)
+		if err != nil {
+			return nil, err
+		}
+		categories = append(categories, c)
+	}
+	return categories,nil
+}
+
+
+func UpdateCategory(id int, name string) (Categories, error) {
+	ctx := context.Background()
+
+	query := `
+		UPDATE categories 
+		SET name = $1, updated_at = NOW()
+		WHERE id = $2
+		RETURNING id, name
+	`
+
+	var c Categories
+
+	err := config.Db.QueryRow(ctx, query, name, id).Scan(&c.Id, &c.Name)
+	if err != nil {
+		return Categories{}, err
+	}
+
+	return c, nil
+}
+
+func DeleteCategory(id int) error {
+	ctx := context.Background()
+
+	query := `DELETE FROM categories WHERE id=$1`
+
+	_, err := config.Db.Exec(ctx, query, id)
 	return err
 }
