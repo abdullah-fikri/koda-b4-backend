2766e066a870d357bcf02da2683e6d5168cca64a
abdullah-fikri <abdullahfikri981@gmail.com>
2025-12-06 07:51:16 +0000

    feat: add controller for crud category

--- controllers/admin.go
+++ controllers/admin.go
@@ -67,3 +67,93 @@ func UpdateOrderStatus(ctx *gin.Context) {
 		Message: "Order status updated",
 	})
 }
+
+
+// create category
+func CreateCategoryController(ctx *gin.Context){
+	var c models.Categories
+	if err := ctx.ShouldBindJSON(&c); err !=nil {
+		ctx.JSON(400, models.Response{
+			Success: false,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	category, err := models.CreateCategory(c.Name)
+	if err != nil {
+		ctx.JSON(500, models.Response{
+			Success: false,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(201, models.Response{
+		Success: true,
+		Message: "Category Created",
+		Data: category,
+	})
+}
+
+// get all categories
+func GetAllCategoriesController(c *gin.Context) {
+	data, err := models.GetAllCategories()
+	if err != nil {
+		c.JSON(500, models.Response{
+			Success: false,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	c.JSON(200, models.Response{
+		Success: true,
+		Message: "list all categories",
+		Data:    data,
+	})
+}
+
+
+// update category
+func UpdateCategoryController(ctx *gin.Context) {
+	idStr := ctx.Param("id")
+	id, _ := strconv.Atoi(idStr)
+
+	var body models.Categories
+
+	if err := ctx.ShouldBindJSON(&body); err != nil {
+		ctx.JSON(400, models.Response{Success: false, Message: err.Error()})
+		return
+	}
+
+	updated, err := models.UpdateCategory(id, body.Name)
+	if err != nil {
+		ctx.JSON(500, models.Response{Success: false, Message: err.Error()})
+		return
+	}
+
+	ctx.JSON(200, models.Response{
+		Success: true,
+		Message: "Category updated",
+		Data:    updated,
+	})
+}
+
+
+// delete 
+func DeleteCategoryController(ctx *gin.Context) {
+	idStr := ctx.Param("id")
+	id, _ := strconv.Atoi(idStr)
+
+	err := models.DeleteCategory(id)
+	if err != nil {
+		ctx.JSON(500, models.Response{Success: false, Message: err.Error()})
+		return
+	}
+
+	ctx.JSON(200, models.Response{
+		Success: true,
+		Message: "Category deleted",
+	})
+}
